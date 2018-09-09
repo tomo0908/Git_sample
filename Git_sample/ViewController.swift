68d7210242df1ec626a6aa9268c9fcec8c3b6b21
太田 <nmioo0829@gmail.com>
2018-09-09 21:34:37 +0900

    ブランチコミット

--- Git_sample/ViewController.swift
+++ Git_sample/ViewController.swift
@@ -31,7 +31,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func makeTextLable() -> UILabel {
+        let textLabel = UILabel(frame: CGRectMake(0, 0, 300, 100))
+        textLabel.layer.position = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
+        textLabel.backgroundColor = UIColor.redColor()
+        textLabel.text = "TECH::CAMP"
+        textLabel.font = UIFont.systemFontOfSize(40)
+        textLabel.textColor = UIColor.whiteColor()
+        textLabel.shadowColor = UIColor.blueColor()
+        textLabel.textAlignment = NSTextAlignment.Center
+        textLabel.layer.masksToBounds = true
+        textLabel.layer.cornerRadius = 20.0
+        return textLabel
+    }
 
 }
 
